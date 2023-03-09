218409389c04ba86f9e0f6724ac95171105942e8
GioKatamadze <giokat.inbox@gmail.com>
2023-03-09 04:22:26 +0400

    fix chatroom get request url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `https://real-time-chat-app-back-production.up.railway.app/api`,
   headers: {
     accept: "application/json",
     "Content-Type": "application/json",
